f1a75d76e4d632e7b35fb4ebd919d0f6859f9397
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2026-02-25 17:23:08 -0500

    fixed tailwind css issues

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // covers src/components, src/pages, etc.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
